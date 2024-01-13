6b7d4f56f7fa8ab031883d83fba3574b396ec079
Wannapa047 <153887983+Wannapa047@users.noreply.github.com>
2024-01-13 22:04:07 +0700

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,54 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.reduce(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  arr.max(2).sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.combination(2).any? { |a, b| a + b == n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  puts "Hello, #{name}!"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  first_char = s.downcase[0]
+  ('a'..'z').include?(first_char) && !'aeiou'.include?(first_char)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false unless s.match?(/^[01]+$/)
+
+  decimal_number = s.to_i(2)
+  decimal_number % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = price
+    validate_input
+  end
+
+  def display_info
+    "ISBN: #{@isbn}, Price: $#{@price}"
+  end
+
+  private
+
+  def validate_input
+    raise ArgumentError, "ISBN should not be empty" if @isbn.empty?
+    raise ArgumentError, "Price should be greater than zero" if @price <= 0
+  end
 end
